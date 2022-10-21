5499eecb77aefb93f09e0b6f58e10cd4cece7a37
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2022-10-21 19:34:30 +0200

    title change

--- web/components/Layout.tsx
+++ web/components/Layout.tsx
@@ -5,7 +5,7 @@ export default function SearchBar({children}): ReactElement {
         <div className="flex items-center justify-center bg-contain w-full h-full">
             <div className="flex-row items-center justify-center bg-739 w-10/12 pb-10 pt-10 my-10 border-solid rounded">
                 <h1 className="text-2xl text-center text-pink-200 m-10 font-light relative">
-                 +++ IMAGE SEARCH ENGINE<sup className="text-xs font-semibold absolute top-0 right-50">powered by AI +++ </sup>
+                 +++ IMAGE SEARCH ENGINE<sup className="text-xs font-semibold absolute top-0 right-50">powered by OpenAI:GPT2 +++ </sup>
                 </h1>
                 <main>{children}</main>
                 <h1 className="text-center text-pink-200 mt-20 font-light"> +++ FAST REMINISCING +++ </h1>
