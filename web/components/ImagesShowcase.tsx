cd7973a7e2dcdfba322f78c2d69aed2e80dfcda5
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2022-10-23 10:00:18 +0200

    small ui changes

--- web/components/ImagesShowcase.tsx
+++ web/components/ImagesShowcase.tsx
@@ -5,7 +5,7 @@ export default function SearchBar(data: {images_url: Array<string>}): ReactEleme
     return (
         <div className="flex justify-evenly flex-wrap overflow-y-scroll w-3/4 h-96 mt-10 mx-auto border border-solid border-pink-200">
             { data.images_url.map((imageSrc) =>
-                <div key={imageSrc} className="flex items-center justify-center m-6 w-fit h-fit overflow-visible border border-dotted border-pink-200 hover:scale-125 transition-transform ease-in-out">
+                <div key={imageSrc} className="flex items-center justify-center m-6 w-fit h-fit overflow-visible hover:cursor-pointer border border-solid border-pink-200 hover:scale-125 transition-transform ease-in-out">
                     <Image src={imageSrc} width={125} height={125}/>
                 </div>
             )}
--- web/components/Layout.tsx
+++ web/components/Layout.tsx
@@ -5,10 +5,11 @@ export default function SearchBar({children}): ReactElement {
         <div className="flex items-center justify-center bg-contain w-full h-full">
             <div className="flex-row items-center justify-center bg-739 w-10/12 pb-10 pt-10 my-10 border-solid rounded">
                 <h1 className="text-2xl text-center text-pink-200 m-10 font-light relative">
-                 +++ IMAGE SEARCH ENGINE<sup className="text-xs font-semibold absolute top-0 right-50">powered by OpenAI:GPT2 +++ </sup>
+                 +++ IMAGE SEARCH ENGINE +++ 
                 </h1>
                 <main>{children}</main>
                 <h1 className="text-center text-pink-200 mt-20 font-light"> +++ FAST REMINISCING +++ </h1>
+                <p className="text-center text-pink-200"><sup className="text-xs font-light"> ⁂ powered by OpenAI:GPT2 </sup></p>
             </div>
         </div>
     );
